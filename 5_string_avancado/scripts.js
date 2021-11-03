fc7328b2a07e2bb24fc2063e9bbfbbc2b1b607d1
JordanoTI <jordanoffice97@gmail.com>
2021-11-03 17:51:55 -0300

    String Avançado

---
+++ 5_string_avancado/scripts.js
@@ -0,0 +1,4 @@
+console.log("Primeira linha \n Segunda linha");
+console.log("O meu nome é 'Jordano'");
+console.log(`A multiplicação de 5 por 3 é: ${5 * 3}`);
+console.log("O " + "meu " + "nome " + "é " + "Jordano");
