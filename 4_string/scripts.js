4b697d287b714a9c669c4c46b1c8cbaaf54ab4dd
JordanoTI <jordanoffice97@gmail.com>
2021-11-03 17:34:39 -0300

    numbers/string

---
+++ 4_string/scripts.js
@@ -0,0 +1,5 @@
+console.log(typeof "Oi, meu nome é Jordano");
+console.log(typeof 'Oi, meu nome é Jordanos');
+console.log(typeof `Teste`);
+console.log(typeof "Infinity");
+console.log(typeof Infinity);
