8a587f97fa84b84a747c32d79c4e99c515e8152c
JordanoTI <jordanoffice97@gmail.com>
2022-01-13 23:07:25 -0300

    convencoes

---
+++ Estruturas de Programação/1_variáveis/scripts.js
@@ -0,0 +1,24 @@
+let nome = "Jordano";
+
+console.log(nome);
+console.log(`O meu nome é ${nome}!`);
+
+let laranja = 5;
+
+console.log(laranja * laranja);
+
+nome = "Jota";
+
+console.log(nome);
+
+laranja = 6;
+
+console.log(laranja);
+
+laranja = "Laranjão Brabo"
+
+console.log(laranja);
+
+let um = 1, dois = 2, tres = 3;
+
+console.log(um + dois + tres);
---
+++ Estruturas de Programação/3_convencao/scripts.js
@@ -0,0 +1,8 @@
+// let 3nome = "teste";
+let nome3 = "teste";
+let $nome = "testee";
+let _nome = "testeee";
+// /* let @nome = "testeee";
+// let !nome = "testeee"; */
+let Nome = "teste";
+let seuNome = "Jota";
