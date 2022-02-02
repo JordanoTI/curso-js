28ca350571743e116b115314561de2ec1e3b45d0
JordanoTI <jordanoffice97@gmail.com>
2022-02-02 10:50:37 -0300

    if_else

---
+++ Estruturas de Programação/10_else/scripts.js
@@ -0,0 +1,7 @@
+let velocidade = 81;
+
+if (velocidade <= 80) {
+  console.log("Não foi multado");
+} else {
+  console.log("Foi multado!");
+}
---
+++ Estruturas de Programação/11_else_if/scripts.js
@@ -0,0 +1,16 @@
+let nome = "Jordano";
+let idade = 51;
+
+if (nome != undefined && nome == "Joaquin") {
+  console.log("Nome esta definido");
+} else if (nome == "Jordano" && nome.length > 5 && idade == 50) {
+  console.log("O nome é Jordano");
+} else {
+  console.log("Não é Jordano");
+}
+
+if (1 > 2) {
+  console.log("teste");
+} else if (1 == 1) {
+  console.log("testando");
+}
