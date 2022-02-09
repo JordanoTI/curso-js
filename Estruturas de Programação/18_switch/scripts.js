d7124d8ce6c30390ac44229179e2c2ad8af3cf0b
JordanoTI <jordanoffice97@gmail.com>
2022-02-09 10:29:16 -0300

    switch

---
+++ Estruturas de Programação/18_switch/scripts.js
@@ -0,0 +1,18 @@
+let nome = "Jordano";
+
+switch (nome) {
+  case "Jordano":
+    console.log("O nome é Jordano");
+    break;
+  case "João":
+    console.log("O nome é João");
+  default:
+    console.log("O nome não foi encontrado");
+    break;
+}
+
+if (nome == "Jordano") {
+  console.log("O nome é Jordano");
+} else {
+  console.log("O nome não foi encontrado");
+}
